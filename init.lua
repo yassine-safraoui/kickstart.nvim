0eb0863cdd0fc50f642f8f03c7d46dcf56337794
Yassine Safraoui <yassine.safraoui@qorvo.com>
2025-03-31 17:45:12 +0200

    current config

--- init.lua
+++ init.lua
@@ -766,6 +766,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        xml = { 'xmllint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
